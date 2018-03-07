2820e76d0c49f568f03ed36b2c9cbda8f6314d9c
Wilson <hrxspp@126.com>
2018-03-07 14:08:59 +0800

    init

---
+++ WilsonFileManager/WilsonFileManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WilsonFileManager
+//
+//  Created by Wilson on 07/03/2018.
+//  Copyright © 2018 Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
